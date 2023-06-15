56f2f69a26b57f21053abec289692afb522f0526
AriFullah3202 <marifullaah@gmail.com>
2023-06-15 23:39:10 +0600

    completed academicFaculty module

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { UserRoute } from '../app/modules/users/user.route';
 import { AcademicSemesterRoute } from '../app/modules/academicSemester/academicSemester.route';
+import { AcademicFacultyRoutes } from '../app/modules/academicFaculty/academicFaculty.route';
 
 const router = express.Router();
 
@@ -12,6 +13,10 @@ const moduleRoutes = [
   {
     path: '/academic-semesters',
     route: AcademicSemesterRoute,
+  },
+  {
+    path: '/academic-faculty',
+    route: AcademicFacultyRoutes,
   },
 ];
 
